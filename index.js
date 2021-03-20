92ba285b055d5c8dc42e924874f33fadd5d57222
DeusAres <exa.xxiv@gmail.com>
2021-03-20 22:34:56 +0100

    Added audio

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let counter = document.querySelector('.counter-display');
 let increment = document.querySelector('.counter-plus');
-let coin = document.querySelector('.coin')
+let coin = document.querySelector('.coin');
+let audio = new Audio('coin.mp3');
 let count = 0
 
 increment.addEventListener("click",()=>{
     count++;
     counter.innerHTML = count;
+    audio.play();
     coin.className = 'coin-jump';
     setTimeout(() => coin.className = 'coin', 400);
 });
